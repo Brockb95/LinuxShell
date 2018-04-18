9058c91ff667264aba313c7c04a1594b5ae22906
RoddieFrederik <35548727+RoddieFrederik@users.noreply.github.com>
2018-04-18 18:01:34 -0500

    Add files via upload
    mycat needs to be moved out of main

--- Project3/mysh.c
+++ Project3/mysh.c
@@ -47,8 +47,6 @@ int num_builtins(){
 	return sizeof(builtin_str) / sizeof(char *);
 }
 
-extern int errno;
-static char * err_str;
 
 
 
@@ -200,22 +198,17 @@ int myexit(){
 int mycat(char ** args) {
 
 	char buffer[256];
-
 	// pipe
 	int fd[2], a;
-
 	if (pipe(fd) < 0)
         exit(1);
 
-
 	if(args[1] == NULL)
 	fprintf(stderr, "mysh: expected argument to \"mycat\"\n");
-	
-	// for the command "mycat filename"
-	else if(args[1] != NULL && args[2] == NULL) {
-		fd[0] = open(args[1], O_RDWR);
-		read(fd[0], buffer, 256);
-		printf("%s", buffer);
+
+	// check if file exists
+	else if(strcmp(args[1], "<") == 0 && open(args[2], O_RDWR) == -1) {
+		printf("mycat: %s: No such file  - Space must be placed between redirection\n", args[2]);
 	}
 
 	// for the command "mycat < file"
@@ -225,31 +218,38 @@ int mycat(char ** args) {
 		printf("%s", buffer);
 	}
 
-
+	// check if file exists
+	else if(open(args[1], O_RDWR) == -1 && args[2] == NULL) {
+		printf("mycat: %s: No such file \n", args[1]);
+	}
 
 	// for the command "mycat < file > file"
-	else if(strcmp(args[3], ">") == 0 && strcmp(args[1], "<") == 0) {
-
+	else if(strcmp(args[1], "<") == 0 && open(args[2], O_RDWR) != -1 && strcmp(args[3], ">") == 0) {
+		if (open(args[4], O_RDWR) == -1) {
+			remove(args[4]);
+		}
 		// open file to write to buffer
 		fd[0] = open(args[2], O_RDWR);
-
 		// read content of file and write to buffer
 		a = read(fd[0], buffer, 256);
-
 		// open file to write from buffer
-		fd[1] = open(args[4], O_RDWR);
-
+		fd[1] = open(args[4], O_RDWR | O_CREAT, 0777);
 		// write to second file
 		write(fd[1], buffer, a-1);
-
-
-		printf("%d", a);
 	}
+
+	
+	// for the command "mycat filename"
+	else if (open(args[1], O_RDWR) != -1 && args[2] == NULL) {
+		fd[0] = open(args[1], O_RDWR);
+		read(fd[0], buffer, 256);
+		printf("%s", buffer);
+	}
+
 
 	else {
-		printf("invalid command, please put a space before and after redirection");
+		printf("mycat: %s: No such file 3  - Space must be placed between redirection\n", args[1]);
 	}
-
 	close(fd[0]);
 	close(fd[1]);
 	return 1;
